c825c784cdcaeaf057ca9e30af8310c26591665f
fardeen-574 <shaiksami1993@gamil.com>
2025-02-15 14:39:45 +0530

    analtic added in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import GradientGenerator from "./GradientGenerator/GradientGenerator.jsx"
+import { Analytics } from '@vercel/analytics/next';
 
 function App() {
 
   return (
     <div >
     <GradientGenerator />
+    <Analytics />
     </div>
   )
 }
